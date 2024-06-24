2cf03c0bca8eef6c9b7acd30ae1e382e37caf247
be.lee <be.lee@univstore.com>
2024-06-24 21:08:38 +0900

    Test: Add SpecialLecture controller /lectures/apply POST e2e

--- src/special_lecture/controller/SpecialLecture.controller.ts
+++ src/special_lecture/controller/SpecialLecture.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { SpecialLectureService } from '../service/SpecialLecture.service';
 import { FindSpecialLectureResponseDTO } from './dto/res/FindSpecialLecture.res.dto';
+import { ApplySpecialLecturesRequestDTO } from './dto/req/ApplySpecialLectures.req.dto';
 
 @Controller('/lectures')
 export class SpecialLectureController {
@@ -9,5 +10,13 @@ export class SpecialLectureController {
   @Get()
   find(): FindSpecialLectureResponseDTO {
     return new FindSpecialLectureResponseDTO();
+  }
+
+  @Post('/apply')
+  apply(
+    @Body(ValidationPipe)
+    applySpecialLectureRequestDTO: ApplySpecialLecturesRequestDTO,
+  ): boolean {
+    return true;
   }
 }
--- test/specialLecture.e2e-spec.ts
+++ test/specialLecture.e2e-spec.ts
@@ -39,4 +39,70 @@ describe('SpecialLectureController (e2e)', () => {
       expect(response.status).toBe(200);
     });
   });
+
+  describe('/lectures/apply (POST): 특강 신청을 한다', () => {
+    test('정상 요청', async () => {
+      //given
+      const body = {
+        userId: 1,
+        lectureScheduleId: 1,
+      };
+      //when
+      const response = await request(app.getHttpServer())
+        .post('/lectures/apply')
+        .send(body);
+      //then
+      expect(response.status).toBe(201);
+    });
+    test('userId가 없을 경우 ERROR를 던진다', async () => {
+      //given
+      const body = {
+        lectureScheduleId: 1,
+      };
+      //when
+      const response = await request(app.getHttpServer())
+        .post('/lectures/apply')
+        .send(body);
+      //then
+      expect(response.status).toBe(400);
+    });
+    test('lectureScheduleId가 없을 경우 ERROR를 던진다', async () => {
+      //given
+      const body = {
+        userId: 1,
+      };
+      //when
+      const response = await request(app.getHttpServer())
+        .post('/lectures/apply')
+        .send(body);
+      //then
+      expect(response.status).toBe(400);
+    });
+    test('userId의 타입이 number 아닐 경우 ERROR를 던진다', async () => {
+      //given
+      const body = {
+        userId: 'string',
+        lectureScheduleId: 1,
+      };
+      //when
+      const response = await request(app.getHttpServer())
+        .post('/lectures/apply')
+        .send(body);
+      //then
+      expect(response.status).toBe(400);
+    });
+    test('lectureScheduleId 타입이 number 아닐 경우 ERROR를 던진다', async () => {
+      //given
+      const body = {
+        userId: 1,
+        lectureScheduleId: 'string',
+      };
+      //when
+      const response = await request(app.getHttpServer())
+        .post('/lectures/apply')
+        .send(body);
+      //then
+      expect(response.status).toBe(400);
+    });
+  });
 });
