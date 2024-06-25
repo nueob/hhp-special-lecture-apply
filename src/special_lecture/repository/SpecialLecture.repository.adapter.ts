5fcfd8341fda8bf5ff925bb85dfae527c5852740
be.lee <be.lee@univstore.com>
2024-06-25 21:52:19 +0900

    Feat: Add SpecialLectures repository and mapper

--- src/special_lecture/repository/SpecialLecture.repository.adapter.ts
+++ src/special_lecture/repository/SpecialLecture.repository.adapter.ts
@@ -1,6 +1,88 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
 import { SpecialLectureRepositoryPort } from '../service/port/SpecialLecture.repository.port';
+import { Lectures } from '../domain/Lectures.domain';
+import { LectureScheduleUsers } from '../domain/LectureScheduleUsers.domain';
+import { LectureEnrollmentRequestLog } from '../domain/LectureEnrollmentRequestLog.domain';
+import { LecturesEntity } from '../../../persistence/entity/lectures.entity';
+import { LectureScheduleUsersEntity } from '../../../persistence/entity/LectureScheduleUsers.entity';
+import { LectureEnrollmentRequestLogEntity } from '../../../persistence/entity/LectureEnrollmentRequestLog.entity';
+import { SpecialLectureMapper } from '../mapper/SpecialLecture.mapper';
 
 @Injectable()
 export class SpecialLectureRepositoryAdapter
-  implements SpecialLectureRepositoryPort {}
+  implements SpecialLectureRepositoryPort
+{
+  constructor(
+    @InjectRepository(LecturesEntity)
+    private readonly lecturesRepository: Repository<LecturesEntity>,
+    @InjectRepository(LectureScheduleUsersEntity)
+    private readonly lectureScheduleUsersRepository: Repository<LectureScheduleUsersEntity>,
+    @InjectRepository(LectureEnrollmentRequestLogEntity)
+    private readonly lectureEnrollmentRequestLogRepository: Repository<LectureEnrollmentRequestLogEntity>,
+  ) {}
+
+  async applyLecture(
+    lectureScheduleUsers: LectureScheduleUsers,
+  ): Promise<LectureScheduleUsers> {
+    const lectureScheduleUsersEntity =
+      this.lectureScheduleUsersRepository.create(
+        SpecialLectureMapper.mapToLectureScheduleUsersEntity(
+          lectureScheduleUsers,
+        ),
+      );
+
+    return SpecialLectureMapper.mapToLectureScheduleUsersDomain(
+      await this.lectureScheduleUsersRepository.save(
+        lectureScheduleUsersEntity,
+      ),
+    );
+  }
+
+  async createHistory(
+    lectureEnrollmentRequestLog: LectureEnrollmentRequestLog,
+  ): Promise<LectureEnrollmentRequestLog> {
+    const lectureEnrollmentRequestLogEntity =
+      this.lectureEnrollmentRequestLogRepository.create(
+        SpecialLectureMapper.mapToLectureEnrollmentRequestLogEntity(
+          lectureEnrollmentRequestLog,
+        ),
+      );
+
+    return SpecialLectureMapper.mapToLectureEnrollmentRequestLogDomain(
+      await this.lectureEnrollmentRequestLogRepository.save(
+        lectureEnrollmentRequestLogEntity,
+      ),
+    );
+  }
+
+  checkEnrolledLectureExistence(
+    userId: number,
+    lectureScheduleId: number,
+  ): Promise<boolean> {
+    return this.lectureScheduleUsersRepository.exists({
+      where: { userId, lectureSchedulesId: lectureScheduleId },
+    });
+  }
+
+  async findAllLectures(): Promise<Lectures[]> {
+    const lectures = await this.lecturesRepository.find({
+      relations: { scheduleList: true },
+    });
+
+    return lectures.map((lecture) =>
+      SpecialLectureMapper.mapToLecturesDomain(lecture),
+    );
+  }
+
+  async findLecture(lectureSchedulesId: number): Promise<Lectures> {
+    return SpecialLectureMapper.mapToLecturesDomain(
+      await this.lecturesRepository.findOne({
+        relations: { scheduleList: { userList: true } },
+        where: { scheduleList: { id: lectureSchedulesId } },
+      }),
+    );
+  }
+}
